9030bb910fd15813e3851b36bce2d0bc3d9cf6e0
amaher <amaher112@su.edu>
2024-03-25 22:07:23 -0400

    created models

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      require: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    reactions: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
