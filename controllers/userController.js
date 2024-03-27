62dee987c33765819ea3fd811745ea39e1d17a20
amaher <amaher112@su.edu>
2024-03-26 22:21:56 -0400

    added update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-const { User, Thought } = require('../models');
-
+const { User, Thought } = require("../models");
 
 module.exports = {
+  // get all users
   async getUsers(req, res) {
     try {
       const users = await User.find();
@@ -10,21 +10,23 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  // get one user
   async getSingleUser(req, res) {
     try {
       const user = await User.findOne({ _id: req.params.userId })
-        .select('-__v')
-        .populate('thoughts');
+        .select("-__v")
+        .populate("thoughts");
 
       if (!user) {
-        return res.status(404).json({ message: 'No user with that ID' });
+        return res.status(404).json({ message: "No user with that ID" });
       }
-
       res.json(user);
     } catch (err) {
       res.status(500).json(err);
     }
   },
+
   // create a new user
   async createUser(req, res) {
     try {
@@ -34,15 +36,35 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  
+
+  // update user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with this id!" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // delete a user
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndRemove({ _id: req.params.userId });
 
       if (!user) {
-        return res.status(404).json({ message: 'No such user exists' })
+        return res.status(404).json({ message: "No such user exists" });
       }
-
       const thought = await Thought.findOneAndUpdate(
         { users: req.params.userId },
         { $pull: { users: req.params.userId } },
@@ -51,15 +73,18 @@ module.exports = {
 
       if (!thought) {
         return res.status(404).json({
-          message: 'User deleted, but no thoughts found',
+          message: "User deleted, but no thoughts found",
         });
       }
 
-      res.json({ message: 'User successfully deleted' });
+      res.json({ message: "User successfully deleted" });
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
 
+  // add a new friend
+
+  // delete a friend
 };
