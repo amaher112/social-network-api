d123b59b7bad66f5632a057a9ac9383000f8e066
amaher <amaher112@su.edu>
2024-03-26 22:22:13 -0400

    created add/delete reaction code

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -5,14 +5,21 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addReaction,
+  deleteReaction,
 } = require("../../controllers/thoughtsController");
 
+// /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
 
+// /api/thoughts/:thoughtId
 router
   .route("/:thoughtId")
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+  router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction)
 
 module.exports = router;
